2235e59a9de9ce8301a04ce8505f1ae625394584
HaydenK19 <104769785+HaydenK19@users.noreply.github.com>
2025-05-02 13:21:10 -0400

    Added more colors for pie chart

--- src/pages/dashboard/budget/cards/visualdata.jsx
+++ src/pages/dashboard/budget/cards/visualdata.jsx
@@ -55,6 +55,36 @@ const VisualCard = () => {
                                 "#C4D2B0", // Light Moss
                                 "#D1C6A1", // Warm Beige
                                 "#B4A798", // Soft Taupe
+                                "#4E7A5F", // Forest Fern
+                                "#6A9A72", // Dusty Jade
+                                "#588868", // Mellow Pine
+                                "#3F6E53", // Deep Sage
+                                "#62876A", // Dull Spruce
+                                "#B2C3AE", // Sage Mist
+                                "#9FAC9B", // Soft Juniper
+                                "#BCCBB9", // Faded Eucalyptus
+                                "#A3B49F", // Gray Sage
+                                "#C0D1BC", // Light Herb
+                                "#5F6F4C", // Shadow Olive
+                                "#758B61", // Dusty Avocado
+                                "#667B52", // Toned Moss
+                                "#7C8D69", // Soft Military
+                                "#586A46", // Woodland Green
+                                "#D0DEC2", // Pale Fern
+                                "#BBD1A6", // Soft Lichen
+                                "#C9D8B5", // Misty Leaf
+                                "#D3E0C4", // Light Herb Green
+                                "#D6E3CC", // Hushed Green
+                                "#DDD2AF", // Sandstone
+                                "#C9BC9B", // Dried Grass
+                                "#E1D7BA", // Linen Gold
+                                "#D9CCA8", // Morning Wheat
+                                "#BFB38C", // Pale Khaki
+                                "#C1B2A3", // Dusty Rose Beige
+                                "#A9998B", // Ash Taupe
+                                "#B7A99A", // Weathered Clay
+                                "#CBBBAE", // Muted Putty
+                                "#AD9E90", // Cocoa Dust
                                 
                             ],
                             borderColor: "#4B6F44", // Dark Green
